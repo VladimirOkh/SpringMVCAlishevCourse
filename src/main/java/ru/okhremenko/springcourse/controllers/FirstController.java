ed4e13d8d48d99cf3c8e86102598e43306f45eae
Vladimir Okhremenko <ohremenkovwork@gmail.com>
2023-07-20 18:32:15 +0300

    add attribute

--- src/main/java/ru/okhremenko/springcourse/controllers/FirstController.java
+++ src/main/java/ru/okhremenko/springcourse/controllers/FirstController.java
@@ -1,8 +1,10 @@
 package ru.okhremenko.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
@@ -10,7 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        //System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message", "Hello " + name + " " + surname);
         return "first/hello";
     }
 
